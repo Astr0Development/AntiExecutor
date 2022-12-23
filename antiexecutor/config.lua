ad98de5503679e76346aa1715364558034b40f3b
Astr0Development <121275860+Astr0Development@users.noreply.github.com>
2022-12-23 05:32:17 +0100

    init antiexecutor

---
+++ antiexecutor/config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+Config.BanEvent = function(source)
+    -- ban event
+    DropPlayer(source, "Executor Detected")
+end
---
+++ antiexecutor/fxmanifest.lua
@@ -0,0 +1,15 @@
+description 'Anti RedEngine 1.0.0 + Anti Dump 1.0.0'
+
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+lua54 'yes'
+
+client_scripts {
+    "client.lua"
+}
+
+server_scripts {
+    "config.lua",
+    "server.lua"
+}
